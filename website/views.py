5c5d342602fda77301c22158a0e5b8c7562bdf23
Danica-Kane <100318768+Danica-Kane@users.noreply.github.com>
2024-05-08 16:24:29 +1200

    foundations side nav

--- website/views.py
+++ website/views.py
@@ -8,3 +8,23 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html", user=current_user)
 
+@views.route('/instant_msg')
+@login_required
+def instant_msg():
+    return render_template("instant_msg.html", user=current_user)
+
+@views.route('/alert_board')
+@login_required
+def alert_board():
+    return render_template("alert_board.html", user=current_user)
+
+@views.route('/event_board')
+@login_required
+def event_board():
+    return render_template("event_board.html", user=current_user)
+
+@views.route('/settings')
+@login_required
+def settings():
+    return render_template("settings.html", user=current_user)
+
