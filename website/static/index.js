8cb8dce66a8d3d3e4ed737a2111183510e498ba7
Danica-Kane <100318768+Danica-Kane@users.noreply.github.com>
2024-07-02 19:46:45 +1200

    refinements - batch 2

--- website/static/index.js
+++ website/static/index.js
@@ -1,3 +1,25 @@
+
+
+/* TOGGLE PASSWORD VISIBILITY */
+
+function showPassword() {
+  var x = document.getElementById("password1");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function showPassword2() {
+  var x = document.getElementById("password2");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
 /* --- ACSESSIBILITY CONTROLS ---  */
 
 /* DYSLEXIA BUTTON TOGGLE FUNCTIONS  */
